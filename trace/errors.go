c123665ea1669a180249438daee75820fd8f462f
Luís Nascimento <luisnascimento.dev@gmail.com>
2024-05-11 23:11:28 -0300

    chore(trace): delete useless errors file

--- trace/errors.go
+++
@@ -1,9 +0,0 @@
-package trace
-
-import "github.com/lcnascimento/go-kit/errors"
-
-// ErrSpanExporterNotSupported indicates that the span exporter used for tracing is not supported.
-var ErrSpanExporterNotSupported = errors.New("span exporter not supported").
-	WithCode("ERR_SPAN_EXPORTER_NOT_SUPPORTED").
-	WithKind(errors.KindInvalidInput).
-	Retryable(false)
