c9a96ff0f26762ee369fa12be8d48bf94516c60f
Luís Nascimento <luis.nascimento@avenue.us>
2024-05-13 09:46:06 -0300

    feat(metric): add useful aliases for commonly used features of OTEL metric package

--- metric/value_objects.go
+++ metric/value_objects.go
@@ -6,3 +6,11 @@ import "go.opentelemetry.io/otel/metric"
 type Meter interface {
 	metric.Meter
 }
+
+// Aliases for commonly used features of OTEL metric package.
+var (
+	WithDescription  = metric.WithDescription
+	WithUnit         = metric.WithUnit
+	WithAttributeSet = metric.WithAttributeSet
+	WithAttributes   = metric.WithAttributes
+)
