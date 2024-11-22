4f4399f873cddf6c4da17fa25a3c50bf189e2e6c
Luís Nascimento <luisnascimento.dev@gmail.com>
2024-11-21 23:44:02 -0300

    feat(runtime): removes propagation dependency

--- runtime/context.go
+++ runtime/context.go
@@ -4,25 +4,7 @@ import (
 	"context"
 	"os"
 	"os/signal"
-
-	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
-
-	"github.com/lcnascimento/go-kit/env"
-	"github.com/lcnascimento/go-kit/propagation"
 )
-
-var (
-	serviceName    = env.GetString("SERVICE_NAME", "unknown")
-	serviceVersion = env.GetString("SERVICE_VERSION", "v0.0.0")
-)
-
-// SystemContextKeySet returns base keys that identifies the current service.
-func SystemContextKeySet() propagation.ContextKeySet {
-	return propagation.ContextKeySet{
-		propagation.ContextKey(semconv.ServiceNameKey):    serviceName,
-		propagation.ContextKey(semconv.ServiceVersionKey): serviceVersion,
-	}
-}
 
 // ContextWithOSSignalCancellation builds a new Context that cancels itself on os.Interrupt signals.
 func ContextWithOSSignalCancellation() context.Context {
