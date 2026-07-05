689b83d6714cd15a20d2a17232b7505ae2ed0371
Luís Nascimento <luisnascimento.comp@gmail.com>
2026-07-05 14:05:36 -0300

    fix(util): fixes error message

--- util/errors.go
+++ util/errors.go
@@ -5,6 +5,6 @@ import "github.com/lcnascimento/go-kit/errors"
 var (
 	ErrParseID         = errors.New("failed to parse id").WithCode("ERR_PARSE_ID")
 	ErrInvalidIDPrefix = func(prefix string) error {
-		return errors.New("id must have prefix '%s'", prefix).WithCode("ERR_INVALID_ID_PREFIX").WithKind(errors.KindInvalidInput)
+		return errors.New("id must have prefix '%s_'", prefix).WithCode("ERR_INVALID_ID_PREFIX").WithKind(errors.KindInvalidInput)
 	}
 )
